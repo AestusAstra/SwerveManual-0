393a057f48f734d2782d792d748b5bb1e968b924
TOM Maekawa <koilamitoad@gmail.com>
2026-03-28 22:19:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches('[data-dropdown-button]');
+  
+  if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
 
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest('[data-dropdown]');
+    currentDropdown.classList.toggle('active'); // Toggle the 'active' class on click
+  }
+
+  document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+    if (dropdown !== currentDropdown) {
+      dropdown.classList.remove('active');
+    }
+  });
+});
