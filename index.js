1f187364aad4a0e154a53a5891f1d2bed8fc89c0
TOM Maekawa <koilamitoad@gmail.com>
2026-07-03 13:16:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
   const menu = document.getElementById("fullScreenMenu");
+  const menubtn = document.getElementByClass("menu-btn");
   if (menu.style.display === "block") {
     menu.style.display = "none";
+    menubtn.style.display = "block";
   } else {
     menu.style.display = "block";
+    menu.style.display = "none";
   }
 }
