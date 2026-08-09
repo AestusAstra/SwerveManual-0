b43c90d0efddf4949ac087505568509f95303c4c
TOM <koilamitoad@gmail.com>
2026-08-09 15:05:22 -0400

    fov goofy

--- indexbarracuda.js
+++ indexbarracuda.js
@@ -9,7 +9,7 @@ scene.background = new THREE.Color(0x000000);
 
 const myDiv = document.getElementById('three')
 
-const camera = new THREE.PerspectiveCamera(fov_in_degrees, myDiv.clientWidth / myDiv.clientHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(90, myDiv.clientWidth / myDiv.clientHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
