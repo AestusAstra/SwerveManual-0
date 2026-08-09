3152062576ce0305a3e9babddcb2c80e5fb41fc4
TOM <koilamitoad@gmail.com>
2026-08-09 14:58:33 -0400

    AAAAAAAAA

--- indexbarracuda.js
+++ indexbarracuda.js
@@ -4,3 +4,23 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
+
+
+
+const myDiv = document.getElementById('three')
+
+const camera = new THREE.PerspectiveCamera(fov_in_degrees, myDiv.clientWidth / myDiv.clientHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(myDiv.clientWidth, myDiv.clientHeight)
+
+myDiv.appendChild(renderer.domElement) 
+
+const loader = new GLTFLoader()
+
+loader.load('path_to_the_model', function (gltf) {
+  const model = gltf.scene;
+  scene.add(model)
+}
+)
