010d942554a7c23b673e909793851510704610f2
TOM <koilamitoad@gmail.com>
2026-08-09 14:15:28 -0400

    i have no clue

--- indexbarracuda.js
+++ indexbarracuda.js
@@ -1,3 +1,6 @@
 import * as THREE from 'three'; 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; 
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
