732db06037ffa81231a0601703a8f7b4c57b38e0
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-08-09 20:01:10 -0700

    Refactor 3D scene setup and model loading
    Updated camera position and rendering settings. Improved model loading and centering logic.

--- indexbarracuda.js
+++ indexbarracuda.js
@@ -5,40 +5,41 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
-
-
 const myDiv = document.getElementById('barracuda');
 
-const camera = new THREE.PerspectiveCamera(90, myDiv.clientWidth / myDiv.clientHeight, 0.1, 1000);
-camera.position.set(0, 2, 5);
+const camera = new THREE.PerspectiveCamera(90, myDiv.clientWidth / myDiv.clientHeight, 0.1, 1000); 
+camera.position.set(0, 0, 0); 
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true }); 
+renderer.setSize(myDiv.clientWidth, myDiv.clientHeight); 
+renderer.setPixelRatio(window.devicePixelRatio); 
+renderer.outputColorSpace = THREE.SRGBColorSpace; 
 
-renderer.setSize(myDiv.clientWidth, myDiv.clientHeight);
+container.appendChild(renderer.domElement); 
 
-myDiv.appendChild(renderer.domElement);
-
-const loader = new GLTFLoader();
-
+const loader = new GLTFLoader(); 
 loader.load(
-  'https://aestusastra.github.io/SwerveManual-0/models/barracuda.glb', 
-  function (gltf) {
-    const model = gltf.scene;
+  '/models/barracuda.glb', 
+  function (gltf) { 
+    const model = gltf.scene; 
+    scene.add(model); 
 
-    /* To Center the model */
-    const box = new THREE.Box3().setFromObject(model);
-    const center = box.getCenter(new THREE.Vector3());
-    model.position.sub(center);
-    
-    scene.add(model);
-  },
+    // Centering the model onto the screen
+    const box = new THREE.Box3().setFromObject(model); 
+    const center = box.getCenter(new THREE.Vector3()); 
+    const size = box.getSize(new THREE.Vector3()); 
+
+    const maxDim = Math.max(size.x, size.y, size.z); 
+    camera.position.set(center.x, center.y + (maxDim * 0.4), maxDim * 2.5); 
+  }, 
   undefined,
-  (error) => { console.error('GLTF Model not working', error); }
-)
+  (error) => { 
+    console.error('Cant load gltf: ', error); 
+  }
+); 
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-animate();
+function animate() { 
+    requestAnimationFrame(animate); 
+    renderer.render(scene, camera); 
+} 
+animate(); 
